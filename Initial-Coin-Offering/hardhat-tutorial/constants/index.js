a136ad74fa5fd35e9e1533065c9adef1a4f42eeb
Tang Weng Kitt <wengkitt969696@gmail.com>
2022-06-06 20:16:53 +0800

    Add Initial-Coin-Offering project

---
+++ Initial-Coin-Offering/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xf0092B267e49B902F8ce6b1C51E7699314f0709A";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
