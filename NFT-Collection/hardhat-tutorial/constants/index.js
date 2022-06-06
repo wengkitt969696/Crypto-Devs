1825e4f99bb81c7f9c2319a6919d9932fa9b9aeb
Tang Weng Kitt <wengkitt969696@gmail.com>
2022-06-06 14:20:13 +0800

    Add NFT-Collection and move white list related file to Whitelist-Dapp

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x23C1C514937144f55b9Ca9E63976E3F695e80376";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
