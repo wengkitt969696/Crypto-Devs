b165b6579abc90f063e7497a79bab34343e95335
Tang Weng Kitt <wengkitt969696@gmail.com>
2022-06-08 22:59:10 +0800

    add DAO

---
+++ DAO/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xf0092B267e49B902F8ce6b1C51E7699314f0709A";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
